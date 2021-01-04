54c8122a904be49f70042d189d55c3515d01fb4e
benku123 <54618998+benku123@users.noreply.github.com>
2021-01-04 16:51:01 -0500

    first commit

--- LinkedList_implementation.py
+++ LinkedList_implementation.py
@@ -7,9 +7,11 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.start_node = None
+        self.index = 0
 
     def append(self, val):
         new = Node(val)
+
         if not self.start_node:
             self.start_node = new
             return
@@ -19,18 +21,39 @@ class LinkedList:
             last = last.ref
         last.ref = new
 
+        self.index += 1
+
+    def insert(self, index, value):
+        start_counting = 0
+        start = self.start_node
+        NewNode = Node(value)
+
+        if index == 0: # Handling the position 0 case.
+            NewNode.ref = start
+            self.start_node = NewNode
+        else:
+            while start_counting < index - 1: # Iterating until the previous node.
+                start_counting += 1
+                start = start.ref
+
+            tmp = start.ref
+            start.ref = NewNode
+            NewNode.ref = tmp
+
+
     def display(self):
+        ls = []
         last = self.start_node
-        while last.ref is not None:
-            print(last.item)
+        while last:
+            ls.append(last.item)
             last = last.ref
-        print(last.item)
+        return ls
 
 
 s = LinkedList()
 s.append(32)
 s.append(23)
+s.append(3222)
 s.append(42)
+s.insert(1, 2)
 print(s.display())
-
-
